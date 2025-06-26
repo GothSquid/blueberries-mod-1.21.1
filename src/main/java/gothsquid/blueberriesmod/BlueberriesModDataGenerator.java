904e573c7e2708c607ac31b122bd745538eaee3f
GothSquid <jmodica29@gmail.com>
2025-06-25 23:52:25 -0500

    Initial commit

---
+++ src/main/java/gothsquid/blueberriesmod/BlueberriesModDataGenerator.java
@@ -0,0 +1,11 @@
+package gothsquid.blueberriesmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlueberriesModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
